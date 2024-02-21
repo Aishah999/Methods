5028c369692511ba2b6518b2ca2766056b2a074a
Aishah999 <aishaah@code.edu.az>
2024-02-22 00:15:30 +0400

    second commit

---
+++ Method/Method/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Method;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        //Console.WriteLine(CheckNumberPrime(8));
+        //Console.WriteLine(IsPower(16));
+    }
+    static string CheckNumberPrime(int num)
+    {
+        int count = default;
+
+        for (int i = 1; i <= num; i++)
+        {
+            if(num%i==0)
+            {
+                count++;
+            }
+        }
+        return count > 2 ? "murekkebdir" : "sadedir";
+    }
+    static bool IsPower(double num)
+    {
+        while (num>1)
+        {
+            num /= 2;
+        }
+        return num == 1 ? true : false;
+    }
+}
+
